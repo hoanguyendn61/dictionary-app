269fecf3bf7677a26e7d786c10e3347fffd22fc2
hungvtm <manhhung01657059072@gmail.com>
2022-05-31 10:09:36 +0700

    added local db package

--- app/src/main/java/com/cuoiky/andoid/dictionaryapp/data/local/AppDatabase.java
+++ app/src/main/java/com/cuoiky/andoid/dictionaryapp/data/local/AppDatabase.java
@@ -1,4 +1,23 @@
 package com.cuoiky.andoid.dictionaryapp.data.local;
 
-public class AppDatabase {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+
+import com.cuoiky.andoid.dictionaryapp.data.model.Word;
+
+@Database(entities = {Word.class}, version = 1)
+@TypeConverters({Converters.class})
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract WordsDao wordsDao();
+    private static AppDatabase instance;
+    public static AppDatabase getInstance(Context context){
+        if(instance == null){
+            instance = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "dictionaryApp").build();
+        }
+        return instance;
+    }
 }
